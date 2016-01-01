208adea8ca989ed682e6c190a5c6c51434c4a619
stevenhao <stevenhao@gmail.com>
2015-12-31 18:51:12 -0800

    improve parsing of practice times
    - handle am/pm better
    - support strings like 12/30 3-5p
      previously, would be parsed as Dec 3, 2030.
    - support strings like 6/4 3-5p
      previously, would be parsed as 3am - 5pm

--- app.js
+++ app.js
@@ -20,6 +20,36 @@ function send(uri, payload, callback) {
   });
 }
 
+// timestr: an interval, e.g. '1/24 3-5pm' or '1/24 3am - 1/31 6pm'
+function parseTimes(timestr) {
+    var times = timestr.split("-");
+    if (times[1].indexOf('/') == -1) {
+        times[1] = times[0].split(' ')[0] + ' ' + times[1];
+    }
+    var re = /.*[apm]$/; // test whether am/pm is supplied
+                         // if not, assume am and adjust as needed
+    function parseDate(str) {
+        // remove ambiguity of 'M/dd h','M/yy d' and 'MM/d h','yy/m d'
+        var exact = Date.parseExact(str, 'M/d h')
+        return exact || Date.parse(str); // using default is safe when format is unambiguous
+    }
+    var starttime = parseDate(times[0], 'M/d h'), startfixed = re.test(times[0]);
+    var endtime = parseDate(times[1], 'M/d h'), endfixed = re.test(times[1]);
+
+    // adjust am/pm
+    var day = 86400000; // 24 * 60 * 60 * 1000
+    var elapsed = endtime - starttime;
+    if (elapsed < 0 && !endfixed) {
+        // handles [11-1 --> 11a-1p]
+        endtime = endtime.add(12).hours();
+    }
+    if (day/2 < elapsed && elapsed < day && !startfixed) {
+        // handles [3-5p --> 3p-5p] (only if within 24 hours)
+        starttime = starttime.add(12).hours();
+    }
+    return [starttime, endtime];
+}
+
 /* Database connection */
 
 var pgp = require('pg-promise')({
@@ -28,21 +58,16 @@ var pgp = require('pg-promise')({
 
 var db = pgp(process.env.DATABASE_URL);
 
+
 // /* Slash command format: /practice 1/24 3-5pm | Barry | throwing and drills */
 var practice = function (req, res, next) {
     var message = req.body.text;
     var pieces = message.split("|");
-    var times = pieces[0].split("-");
     var practiceLocation = pieces[1];
     var description = pieces[2];
-    var starttime = Date.parse(times[0]);
-    var endtime = Date.parse(times[1]);
-
-    if (endtime.getDate() == Date.today().getDate()) {
-        endtime.setMonth(starttime.getMonth());
-        endtime.setDate(starttime.getDate());
-    }
-    
+    var times = parseTimes(pieces[0]);
+    var starttime = times[0];
+    var endtime = times[1];
     var format = 'MM-dd-yy HH:mm:ss',
         query = "INSERT INTO practices (starttime, endtime, location, message) VALUES ($1, $2, $3, $4) RETURNING id";
 
@@ -83,7 +108,7 @@ var respondWithPage = function(res, fileName, hash) {
 };
 
 var printTime = function(starttime, endtime) {
-    return starttime.toString("dddd M/d h:mm") + "-" + endtime.toString("h:mm tt");
+    return starttime.toString("dddd M/d h:mm tt") + "-" + endtime.toString("h:mm tt");
 };
 
 var view_practice = function(req, res, next) {
